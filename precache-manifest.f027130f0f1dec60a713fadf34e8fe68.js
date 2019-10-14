cc47048f5fde84eeabdf36e0f18c1abfb7273dd2
alexander <a.samoyl11@gmail.com>
2019-10-14 23:37:10 +0300

    Updates

---
+++ precache-manifest.f027130f0f1dec60a713fadf34e8fe68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "bed05b11ff184846e098",
+    "url": "./static/js/main.bed05b11.chunk.js"
+  },
+  {
+    "revision": "9d3d3482eef8808a8ee0",
+    "url": "./static/js/1.9d3d3482.chunk.js"
+  },
+  {
+    "revision": "bed05b11ff184846e098",
+    "url": "./static/css/main.6036996c.chunk.css"
+  },
+  {
+    "revision": "9d3d3482eef8808a8ee0",
+    "url": "./static/css/1.23ebf9f1.chunk.css"
+  },
+  {
+    "revision": "7064d157028ca06c5d2b8eafda88c407",
+    "url": "./index.html"
+  }
+];
